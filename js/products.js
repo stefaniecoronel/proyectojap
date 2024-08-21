f3c0763dd1b25b2bbbae1f41a320d3ac27c6976a
stefaniecoronel <stefaniecoronel15@gmail.com>
2024-08-21 04:07:39 -0300

    Cambio para utilizar getJSONData

--- js/products.js
+++ js/products.js
@@ -1,30 +1,34 @@
-document.addEventListener('DOMContentLoaded', function () {
-let contenedor = document.getElementById('listado-articulos');
 
-fetch("https://japceibal.github.io/emercado-api/cats_products/101.json")
-.then(response => response.json())
-.then(data => {
-    data.products.forEach( articulo => {
-        const columna = document.createElement('div');
-        columna.className = 'col-md-4';
-        columna.innerHTML = `
+document.addEventListener('DOMContentLoaded', function(){
+  getJSONData("https://japceibal.github.io/emercado-api/cats_products/101.json").then(function(respObj){
+    if(respObj.status == "ok"){
+      console.log(respObj.data.products)
+      cargarArticulos(respObj.data.products)
+      
+    }
+  })
+  
+})
+let contenedor=document.getElementById('listado-articulos')
+function cargarArticulos (arreglo){
+  arreglo.forEach((element) =>{
+    contenedor.innerHTML += `
         <div class="card mb-3" style="max-width: 540px;">
         <div class="row g-0">
         <div class="col-md-4">
-          <img src="${articulo.image}" class="img-fluid rounded-start" alt="${articulo.name}">
+          <img src="${element.image}" class="img-fluid rounded-start" alt="${element.name}">
         </div>
         <div class="col-md-8">
           <div class="card-body">
-            <h5 class="card-title">${articulo.name}</h5>
-            <p class="card-text">${articulo.description}</p>
-            <h5 class="card-title">${articulo.currency} ${articulo.cost}</h5>
-            <p class="card-text"><small class="text-muted">${articulo.soldCount} unidades vendidas.</small></p>
+            <h5 class="card-title">${element.name}</h5>
+            <p class="card-text">${element.description}</p>
+            <h5 class="card-title">${element.currency} ${element.cost}</h5>
+            <p class="card-text"><small class="text-muted">${element.soldCount} unidades vendidas.</small></p>
           </div>
         </div>
       </div>
-    </div>`;
-    contenedor.appendChild(columna);
-   
-    });
-})
-});
+    </div>`
+
+  }
+)
+}
