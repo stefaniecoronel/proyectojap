a2b083fd09bfe8880fb361adb965f9c973b8b214
stefaniecoronel <stefaniecoronel15@gmail.com>
2024-08-22 12:29:07 -0300

    Update login.js
    Botón ingresar que si los campos de usuario y contraseña se completan redirige a la página de inicio.

--- js/login.js
+++ js/login.js
@@ -1,10 +1,14 @@
-let button =  document.getElementById ("botonLogin2");
-let usuario = document.getElementById ("usuario").value;
-let contraseña = document.getElementById ("contraseña").value;
+let form= document.querySelector("form")
 
-button.addEventListener ('click',function(){
+
+form.addEventListener ('submit',function(event){
+    event.preventDefault();
+    let usuario = document.getElementById ("usuario").value;
+    let contraseña = document.getElementById ("contraseña").value;
     if (usuario && contraseña) {
+        form.submit();
         window.location.href="index.html";
+
     }
 } )
 
