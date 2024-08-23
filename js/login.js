2d9e9d2d41df9a40791724f4281790ca58c786aa
Sofia Olivera <sogon2009@gmail.com>
2024-08-23 12:13:45 -0300

    desafiate

--- js/login.js
+++ js/login.js
@@ -1,14 +1,28 @@
+
 let form= document.querySelector("form")
 
 
-form.addEventListener ('submit',function(event){
+document.addEventListener ('submit',function(event){
     event.preventDefault();
     let usuario = document.getElementById ("usuario").value;
     let contraseña = document.getElementById ("contraseña").value;
     if (usuario && contraseña) {
-        form.submit();
+    
+        localStorage.setItem('session', 'active');
         window.location.href="index.html";
 
     }
 } )
 
+
+function checkSession() {
+    const session = localStorage.getItem('session');
+    if (session === 'active') {
+        console.log('Sesión activa.');
+    } else {
+        console.log('No hay sesión activa.');
+        
+            window.location.href = 'login.html'; // Redirige después de un breve retraso   
+}
+}
+window.onload = checkSession;
