7fb1cb98cf663988b002ddc02a0289cfaf4f2438
stefaniecoronel <stefaniecoronel15@gmail.com>
2024-09-11 17:56:18 -0300

    Se agrega la funcionalidad a los botones para filtrar por precio.

--- js/products.js
+++ js/products.js
@@ -3,38 +3,74 @@ document.addEventListener('DOMContentLoaded', function(){
   let catID = localStorage.getItem("catID")
   console.log (catID)
   let direccion =`https://japceibal.github.io/emercado-api/cats_products/${catID}.json`
+  let productos = []
   getJSONData(direccion).then(function(respObj){
     if(respObj.status == "ok"){
       console.log(respObj.data.products)
-      cargarArticulos(respObj.data.products)
+      productos = respObj.data.products
+      cargarArticulos(productos)
       cargarCategoria(respObj.data.catName)
 
     }
   })
-  
+  document.getElementById("clearRangeFilter").addEventListener("click", function(){
+    document.getElementById("rangeFilterPriceMin").value = "";
+    document.getElementById("rangeFilterPriceMax").value = "";
+
+    minCost = undefined;
+    maxCost = undefined;
+
+    cargarArticulos(productos);
+  });
+
+document.getElementById("rangeFilterCount").addEventListener("click", function(){
+    //Obtengo el mínimo y máximo de los intervalos para filtrar por precio.
+    minCost = document.getElementById("rangeFilterPriceMin").value;
+    maxCost = document.getElementById("rangeFilterPriceMax").value;
+
+    if ((minCost != undefined) && (minCost != "") && (parseInt(minCost)) >= 0){
+        minCost = parseInt(minCost);
+    }
+    else{
+        minCost = undefined;
+    }
+
+    if ((maxCost != undefined) && (maxCost != "") && (parseInt(maxCost)) >= 0){
+        maxCost = parseInt(maxCost);
+    }
+    else{
+        maxCost = undefined;
+    }
+
+    cargarArticulos(productos);
+  });
 })
 
 
 let contenedor=document.getElementById('listado-articulos')
 function cargarArticulos (arreglo){
+  contenedor.innerHTML = ""
   arreglo.forEach((element) =>{
-    contenedor.innerHTML += `
-    <div class="col-lg-5 justify-content-center d-flex">
-        <div class="card mb-2 h-100" style="max-width: 540px;">
-        <div class="row g-0 h-100">
-        <div class="col-md-4">
-          <img src="${element.image}" class="img-fluid rounded-start h-100" alt="${element.name}">
-        </div>
-        <div class="col-md-8 d-flex flex-column justify-content-between">
-          <div class="card-body">
-            <h5 class="card-title">${element.name}</h5>
-            <p class="card-text">${element.description}</p>
-            <h5 class="card-title">${element.currency} ${element.cost}</h5>
-            <p class="card-text mt-auto"><small class="text-muted">${element.soldCount} unidades vendidas.</small></p>
-          </div>
-        </div>
-      </div>
-    </div>`
+    if (((minCost == undefined) || (minCost != undefined && parseInt(element.cost) >= minCost)) &&
+        ((maxCost == undefined) || (maxCost != undefined && parseInt(element.cost) <= maxCost))) {
+          contenedor.innerHTML = `
+          <div class="col-lg-5 justify-content-center d-flex">
+              <div class="card mb-2 h-100" style="max-width: 540px;">
+              <div class="row g-0 h-100">
+              <div class="col-md-4">
+                <img src="${element.image}" class="img-fluid rounded-start h-100" alt="${element.name}">
+              </div>
+              <div class="col-md-8 d-flex flex-column justify-content-between">
+                <div class="card-body">
+                  <h5 class="card-title">${element.name}</h5>
+                  <p class="card-text">${element.description}</p>
+                  <h5 class="card-title">${element.currency} ${element.cost}</h5>
+                  <p class="card-text mt-auto"><small class="text-muted">${element.soldCount} unidades vendidas.</small></p>
+                </div>
+              </div>
+            </div>
+          </div>`
+        }
 
   })}
   let contenedorTitulo=document.getElementById('div-titulo-cat')
@@ -54,28 +90,28 @@ function cargarArticulos (arreglo){
   const ORDER_BY_PROD_REL = "Rel";
   let currentCategoriesArray = [];
   let currentSortCriteria = undefined;
-  let minCount = undefined;
-  let maxCount = undefined;
+  let minCost = undefined;
+  let maxCost = undefined;
   
   function sortCategories(criteria, array){
       let result = [];
       if (criteria === ORDER_ASC_BY_PRICE)
       {
           result = array.sort(function(a, b) {
-              if ( a.name < b.name ){ return -1; }
-              if ( a.name > b.name ){ return 1; }
+              if ( a.cost < b.cost ){ return -1; }
+              if ( a.cost > b.cost ){ return 1; }
               return 0;
           });
       }else if (criteria === ORDER_DESC_BY_PRICE){
           result = array.sort(function(a, b) {
-              if ( a.name > b.name ){ return -1; }
-              if ( a.name < b.name ){ return 1; }
+              if ( a.cost > b.cost ){ return -1; }
+              if ( a.cost < b.cost ){ return 1; }
               return 0;
           });
       }else if (criteria === ORDER_BY_PROD_REL){
           result = array.sort(function(a, b) {
-              let aCount = parseInt(a.productCount);
-              let bCount = parseInt(b.productCount);
+              let aCount = parseInt(a.soldCount);
+              let bCount = parseInt(b.soldCount);
   
               if ( aCount > bCount ){ return -1; }
               if ( aCount < bCount ){ return 1; }
