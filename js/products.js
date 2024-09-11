3248daad36cf7af8905a1c6117e12e6ef5a8dbda
solana-28 <soli.v.2801@gmail.com>
2024-09-11 16:13:09 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -62,20 +62,20 @@ function cargarArticulos (arreglo){
       if (criteria === ORDER_ASC_BY_PRICE)
       {
           result = array.sort(function(a, b) {
-              if ( a.name < b.name ){ return -1; }
-              if ( a.name > b.name ){ return 1; }
+              if ( a.cost< b.cost ){ return -1; }
+              if ( a.cost > b.cost ){ return 1; }
               return 0;
           });
       }else if (criteria === ORDER_DESC_BY_PRICE){
           result = array.sort(function(a, b) {
-              if ( a.name > b.name ){ return -1; }
-              if ( a.name < b.name ){ return 1; }
+              if ( a.cost > b.cost ){ return -1; }
+              if ( a.cost < b.cost ){ return 1; }
               return 0;
           });
       }else if (criteria === ORDER_BY_PROD_REL){
           result = array.sort(function(a, b) {
-              let aCount = parseInt(a.productCount);
-              let bCount = parseInt(b.productCount);
+              let aCount = parseInt(a.sortCount);
+              let bCount = parseInt(b.sortCount);
   
               if ( aCount > bCount ){ return -1; }
               if ( aCount < bCount ){ return 1; }
@@ -85,5 +85,5 @@ function cargarArticulos (arreglo){
   
       return result;
   } 
-
   
+
