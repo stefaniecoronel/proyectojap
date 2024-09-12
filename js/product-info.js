69a4a38cdabc6d23eb963d5b05db3320b90ec7b2
stefaniecoronel <stefaniecoronel15@gmail.com>
2024-09-12 18:05:30 -0300

    Se agrega la categoría.
    Se logra ahora sí que el carousel  no se mueva solo.

--- js/product-info.js
+++ js/product-info.js
@@ -2,13 +2,14 @@ document.addEventListener('DOMContentLoaded', function(){
     let productID = localStorage.getItem("productID")
     console.log (productID)
     let direccion =`https://japceibal.github.io/emercado-api/products/${productID}.json`
-    //Cargo en la página los artículos a través del JSON.
     getJSONData(direccion).then(function(respObj){
     if(respObj.status == "ok"){
         console.log(respObj.data)
         infoProduct = respObj.data
         cargarProducto(infoProduct)
+        cargarCategoriaBreadcrumb(respObj.data)
     }
+
 });
 });
 
@@ -20,20 +21,33 @@ function cargarProducto(producto){
     imagenes.innerHTML = "" ;
     if (arregloImagenes.length>0){
         imagenes.innerHTML +=`<div class="carousel-item active">
-        <img src=${arregloImagenes[0]} class="d-block w-100" alt="auto">
+        <img src=${arregloImagenes[0]} class="d-block w-100" alt=${producto.name}>
         </div>`
         for(let i = 1; i < arregloImagenes.length; i++){
         imagenes.innerHTML+=`<div class="carousel-item">
-        <img src=${arregloImagenes[i]} class="d-block w-100" alt="auto">
+        <img src=${arregloImagenes[i]} class="d-block w-100" alt=${producto.name}>
         </div>`
         }
     }    
     contenedorInfo.innerHTML = `<div class="card-body">
         <h5 class="card-title">${producto.name}</h5>
+        <br>
         <p class="card-text">${producto.description}</p>
+        <br>
         <h5 class="card-title">${producto.currency} ${producto.cost}</h5>
+        <br>
         <p class="card-text mt-auto"><small class="text-muted">${producto.soldCount} unidades vendidas.</small></p>
         </div>`
 };
 
+let contenedorBreadcrumb = document.getElementById('breadcrumb-cat')
 
+function cargarCategoriaBreadcrumb(producto){
+    contenedorBreadcrumb.innerHTML +=`<nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="categories.html">Categorías</a></li>
+    <li class="breadcrumb-item"><a href="products.html">${producto.category}</a></li>
+    <li class="breadcrumb-item active" aria-current="page">${producto.name}</a></li>
+  </ol>
+</nav>`
+  }
