8baa24e639d30b0b142011f1040a4061ab70b0e7
stefaniecoronel <stefaniecoronel15@gmail.com>
2024-09-11 21:03:36 -0300

    Funcionalidad botones
    Se agrega funcionalidad a los botones que permiten ordenar por precio ascendente, descendente y por relevancia.

--- js/products.js
+++ js/products.js
@@ -3,28 +3,42 @@ document.addEventListener('DOMContentLoaded', function(){
   let catID = localStorage.getItem("catID")
   console.log (catID)
   let direccion =`https://japceibal.github.io/emercado-api/cats_products/${catID}.json`
-  let productos = []
+  //Cargo en la página los artículos a través del JSON.
   getJSONData(direccion).then(function(respObj){
     if(respObj.status == "ok"){
       console.log(respObj.data.products)
-      productos = respObj.data.products
-      cargarArticulos(productos)
+      currentProductsArray = respObj.data.products
+      cargarArticulos(currentProductsArray)
       cargarCategoria(respObj.data.catName)
 
     }
   })
+  //Agrego la funcionalidad de ordenar de forma ascendente al correspondiente botón.
+  document.getElementById("sortAsc").addEventListener("click", function(){
+    ordenarYCargarArticulos(ORDER_ASC_BY_PRICE);
+  });
+  //Agrego la funcionalidad de ordenar de forma descendente al correspondiente botón.
+  document.getElementById("sortDesc").addEventListener("click", function(){
+    ordenarYCargarArticulos(ORDER_DESC_BY_PRICE);
+  });
+  //Agrego la funcionalidad de ordenar por relevancia al correspondiente botón.
+  document.getElementById("sortByCount").addEventListener("click", function(){
+    ordenarYCargarArticulos(ORDER_BY_PROD_REL);
+  });
+
   document.getElementById("clearRangeFilter").addEventListener("click", function(){
+    //Al hacer click en el botón limpiar minCost y maxCost pasan a ser undefined.
     document.getElementById("rangeFilterPriceMin").value = "";
     document.getElementById("rangeFilterPriceMax").value = "";
 
     minCost = undefined;
     maxCost = undefined;
 
-    cargarArticulos(productos);
+    cargarArticulos(currentProductsArray);
   });
 
 document.getElementById("rangeFilterCount").addEventListener("click", function(){
-    //Obtengo el mínimo y máximo de los intervalos para filtrar por precio.
+    //Obtengo el mínimo y máximo de los intervalos para poder filtrar por precio al usar la función cargarArticulos.
     minCost = document.getElementById("rangeFilterPriceMin").value;
     maxCost = document.getElementById("rangeFilterPriceMax").value;
 
@@ -41,19 +55,21 @@ document.getElementById("rangeFilterCount").addEventListener("click", function()
     else{
         maxCost = undefined;
     }
-
-    cargarArticulos(productos);
+    cargarArticulos(currentProductsArray);
   });
 })
 
 
 let contenedor=document.getElementById('listado-articulos')
+//Esta función carga los artículos a partir de un arreglo.
 function cargarArticulos (arreglo){
   contenedor.innerHTML = ""
   arreglo.forEach((element) =>{
+    //Por cada elemento del arreglo me fijo si está definido el rango de precios y si este queda
+    //dento del mismo.
     if (((minCost == undefined) || (minCost != undefined && parseInt(element.cost) >= minCost)) &&
         ((maxCost == undefined) || (maxCost != undefined && parseInt(element.cost) <= maxCost))) {
-          contenedor.innerHTML = `
+          contenedor.innerHTML += `
           <div class="col-lg-5 justify-content-center d-flex">
               <div class="card mb-2 h-100" style="max-width: 540px;">
               <div class="row g-0 h-100">
@@ -75,6 +91,8 @@ function cargarArticulos (arreglo){
   })}
   let contenedorTitulo=document.getElementById('div-titulo-cat')
   let contenedorBreadcrumb=document.getElementById('breadcrumb-cat')
+  //Esta función pone el título a la página, este corresponde a la categoría de artículos. 
+  //También coloca la categoría de artículos al breadcrumb.
   function cargarCategoria(categoria){
     contenedorTitulo.innerHTML +=`<h2>${categoria}</h2>`
     contenedorBreadcrumb.innerHTML +=`<nav aria-label="breadcrumb">
@@ -88,12 +106,14 @@ function cargarArticulos (arreglo){
   const ORDER_ASC_BY_PRICE = "Az";
   const ORDER_DESC_BY_PRICE = "Za";
   const ORDER_BY_PROD_REL = "Rel";
-  let currentCategoriesArray = [];
+  let currentProductsArray = [];
   let currentSortCriteria = undefined;
   let minCost = undefined;
   let maxCost = undefined;
   
-  function sortCategories(criteria, array){
+  //Esta función ordena el array que contiene los artículos según distintos criterios:
+  //precio ascendente, descendente y relevancia (definida según cantidad de vendidos)
+  function ordenarArticulos(criteria, array){
       let result = [];
       if (criteria === ORDER_ASC_BY_PRICE)
       {
@@ -121,5 +141,18 @@ function cargarArticulos (arreglo){
   
       return result;
   } 
+  //Esta función recibe un criterio para ordenar los articulos y un arreglo de los mismos.
+  //Permite ordenarlos utilizando la función ordenarArtículos y mostrarlos usando cargarArticulos.
+  function ordenarYCargarArticulos(sortCriteria, productsArray){
+    currentSortCriteria = sortCriteria; //defino el criterio para ordenar
 
+    if(productsArray != undefined){
+        currentProductsArray = productsArray;
+    } //defino el array de artículos
+
+    currentProductsArray = ordenarArticulos(currentSortCriteria, currentProductsArray); //ordeno los artículos
+
+    //Una vez ordenados los artículos, los muestro y tengo en cuenta intervalo de precios con la siguiente función:
+    cargarArticulos(currentProductsArray);
+}
   
