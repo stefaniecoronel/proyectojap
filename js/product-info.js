5cdfc1c4bdb4bae9e3cc1a5ba39a50927eace5cc
stefaniecoronel <stefaniecoronel15@gmail.com>
2024-09-30 06:49:25 -0300

    Comentarios
    Se agregan explicaciones de código.

--- js/product-info.js
+++ js/product-info.js
@@ -20,29 +20,37 @@ document.addEventListener('DOMContentLoaded', function(){
     })
 
   });
-//Función para desafiate en proceso.
+//Al hacer click en el botón enviar del formulario se evalua si hay contenido en el textarea o 
+//alguna estrella seleccionada. Si algo de esto ocurre se guarda el comentario en el localStorage.
 document.getElementById("enviar-comentario").addEventListener('click', function(){
   let comentarioNuevo = document.getElementById('comentario-nuevo').value
   let ratings = document.getElementsByName('rating') 
   let fechaComentario = formatearFecha()
   let nombreUsuario = localStorage.getItem('nombre')
-  let producto =  localStorage.getItem('productID')
+  let producto =  localStorage.getItem('productID') // Se guarda este dato para que luego los comentarios realizados solo aparezcan en la página del producto al que corresponden.
   let userRating = 0
   if (comentarioNuevo || (document.querySelector('input[name="rating"]:checked')) ) {
+    //El for se utiliza para saber cuantas estrellas se le puso al producto en función del value del radio button seleccionado.
     for (let i=0; i< ratings.length; i++){
       if (ratings[i].checked){
           userRating = ratings[i].value
           break; 
       }
     }
+    //Se define un objeto con todos los datos del comentario.
     let review = {identificador:producto, usuario:nombreUsuario, comentario:comentarioNuevo, rating:userRating, fecha:fechaComentario}
     let comentariosRealizados;
+    // Se chequea si hay datos guardados en el local storage con la key 'comentarios-realizados', si los hay
+    // se los almacena en formato de array en la variable comentariosRealizados, si no los hay se define dicha variable
+    // como un array vacío.
     if (localStorage.getItem('comentarios-realizados')){
       comentariosRealizados = JSON.parse(localStorage.getItem('comentarios-realizados'));
     } else {
       comentariosRealizados = [];
     }
-    comentariosRealizados.push(review)
+
+    comentariosRealizados.push(review) //Se agrega el comentario realizado al array de comentarios.
+    //Se guarda el array de comentarios luego de convertirlo a formato JSON en el local storage.
     localStorage.setItem('comentarios-realizados', JSON.stringify(comentariosRealizados))
     
   
@@ -51,7 +59,7 @@ document.getElementById("enviar-comentario").addEventListener('click', function(
   else { 
     alert ("Debe ingresar un comentario o calificación.")
   }
-  x
+  
 });
 agregarComentario();
 });
@@ -59,6 +67,7 @@ agregarComentario();
 let contenedorInfo = document.getElementById("info");
 let imagenes = document.getElementById("imagenes");
 
+//Esta función se utiliza para cargar el producto en un formato de carousel además de su información como una card.
 function cargarProducto(producto){
     let arregloImagenes = producto.images
     imagenes.innerHTML = "" ;
@@ -97,6 +106,7 @@ function cargarCategoriaBreadcrumb(producto){
 
 let contenedorComentario = document.getElementById('comentarios-contenedor')
 
+//Esta función se utiliza para cargar los comentarios asociados a cada producto que se encuentran en el JSON.
 function cargarComentarios(comentarios){
   contenedorComentario.innerHTML=""
   comentarios.forEach(element => {
@@ -115,6 +125,8 @@ function cargarComentarios(comentarios){
   });
 }
 let contenedorComentariosNuevos = document.getElementById('comentarios-nuevos-contenedor')
+//Esta función se utiliza para agregar los comentarios realizados mediante la sección de enviar comentario.
+//Agrega todos los comentarios que se guardaron en el local storage con la key 'comentarios-realizados'.
 function agregarComentario()  {
   productoActual = localStorage.getItem('productID')
   contenedorComentariosNuevos.innerHTML = "";
@@ -135,9 +147,9 @@ function agregarComentario()  {
   )
   };
 
+  let contenedorProdRelacionados = document.getElementById('productos-relacionados')
 
-let contenedorProdRelacionados = document.getElementById('productos-relacionados')
-
+//Esta función se utiliza para añadir a la página los productos relacionados mediante un formato de cards.
 function cargarProductosRelacionados (prodRelacionados){
   contenedorProdRelacionados.innerHTML=""
   prodRelacionados.forEach(element => {
@@ -152,6 +164,8 @@ function cargarProductosRelacionados (prodRelacionados){
   });
 }
 
+//Esta función retorna una porción de código html con la cantidad de estrellas correspondientes
+//a un puntaje.
 function showStars (stars) {
   let starRating ="";
   if (stars<=1.2){
@@ -221,20 +235,21 @@ function showStars (stars) {
  return starRating;
 };
 
+// Esta función permite retornar la fecha y hora de los comentarios agregados con el
+//formato de fecha que utilizan los comentarios que se encuentran en el JSON.
 function formatearFecha(){
- let fecha = new Date()
-  // Obtener cada parte de la fecha y hora
+ let fecha = new Date() // Se guarda fecha y hora al momento de que se hace el comentario.
+ // Se obtiene cada parte de la fecha y la hora.
  let year = fecha.getFullYear();
- let month = String(fecha.getMonth() + 1).padStart(2, '0'); // Añadir cero delante si es necesario
+ let month = String(fecha.getMonth() + 1).padStart(2, '0'); //Se utiliza padStart para agregar un 0 a la izquierda si es necesario.
  let day = String(fecha.getDate()).padStart(2, '0');
 
  let hours = String(fecha.getHours()).padStart(2, '0');
  let minutes = String(fecha.getMinutes()).padStart(2, '0');
  let seconds = String(fecha.getSeconds()).padStart(2, '0');
-
- // Formatear la fecha y hora
- let formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
- return formattedDate
+ //Se rearma la fecha en el formato deseado.
+ let fechaFormateada = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+ return fechaFormateada
 }
 
 function setProductID(id) {
