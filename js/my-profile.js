f154f564988016c4ca7eb2bea2bc701af4ff4266
stefaniecoronel <stefaniecoronel15@gmail.com>
2024-10-13 01:53:03 -0300

    Memoria botón switch
    El botón switch aparece en la posición en que estaba la última vez que el usuario guardó cambios.

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,9 +1,19 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-    let switchNightMode = document.getElementById('SwitchNightMode')
-    let profileForm = document.getElementById('profileForm')
+    let switchNightMode = document.getElementById('SwitchNightMode');
+    let profileForm = document.getElementById('profileForm');
+
+    switchNightModeStatus = localStorage.getItem('nightMode')
     
-        profileForm.addEventListener('submit', function(event){
+    //Muestro el botón switch en el último estado que dejó el usuario al guardar cambios 
+    if(switchNightModeStatus === 'enabled'){
+        switchNightMode.checked = true; 
+    } else {
+        switchNightMode.checked = false; 
+    }
+    
+    // Guardo lo que el usuario envie como preferencia de modo (día o noche) en el local storage
+    profileForm.addEventListener('submit', function(event){
             event.preventDefault();
         if (switchNightMode.checked){
             localStorage.setItem('nightMode', 'enabled');
