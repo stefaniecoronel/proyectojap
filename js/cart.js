4d8a65c711bff8e0969be4724ac855079a9f0e18
stefaniecoronel <stefaniecoronel15@gmail.com>
2024-11-02 12:07:38 -0300

    Opción eleminar artículos
    Se agrega la opción de eliminar artículos del carrito.

--- js/cart.js
+++ js/cart.js
@@ -7,68 +7,12 @@ if (productosCarrito){
 
 localStorage.setItem('producto-carrito', JSON.stringify(productosCarrito))
 
-document.addEventListener('DOMContentLoaded', function(){
-
 let contenedorProductosCarrito = document.getElementById('contenedor-productos-carrito')
 
-if (!productosCarrito){
-    contenedorProductosCarrito.innerHTML = `
-    <div class="alert alert-primary text-center">
-    No hay productos en su carrito aún.
-    </div> `
-} else {
 
-  productosCarrito.forEach((productoCarrito) => { 
-    contenedorProductosCarrito.innerHTML += `<div class="justify-content-center d-flex">
-    <div class="card">
-    <div class="row">
-    <div class="col-md-4">
-      <img src="${productoCarrito.image}" class="img-fluid rounded-start h-100" alt="${productoCarrito.name}">
-    </div>
-    <div class="col-md-8 d-flex flex-column justify-content-between">
-      <div class="card-body">
-        <h5 class="card-title">${productoCarrito.name}</h5>
-        <h5 class="card-title">${productoCarrito.currency} ${productoCarrito.cost}</h5>
-        <input type="number" id="cantidad-${productoCarrito.id}" class="form-control w-25" min="1"  value="${productoCarrito.cantidad}">
-        <p class="card-text">Subtotal: <span id="subtotal-${productoCarrito.id}">${productoCarrito.cost}</span></p>
-        
-      </div>
-    </div>
-    </div>
-    </div>`
+document.addEventListener('DOMContentLoaded', function(){
 
-    
-  });
-
-  
-
-}
-
-if (productosCarrito){
-  productosCarrito.forEach((productoCarrito, index) => {
-    let inputCantidad = document.getElementById(`cantidad-${productoCarrito.id}`);
-    let subtotalElemento = document.getElementById(`subtotal-${productoCarrito.id}`);
-  
-    
-  
-    // Escucha cambios en el input de cantidad
-    inputCantidad.addEventListener('input', function() {
-        let cantidad = parseInt(inputCantidad.value) || 1; // Asegura que cantidad sea al menos 1
-        let subtotal = productoCarrito.cost * cantidad;
-        subtotalElemento.textContent = subtotal;
-        
-        productosCarrito[index] = { ...productosCarrito[index], cantidad };
-  
-        localStorage.setItem('producto-carrito', JSON.stringify(productosCarrito));
-  
-        updateTotals();
-        
-   });
-   
-  
-  });
-}
-
+cargarCarrito ();
 
 let contenedorTotalCompra = document.getElementById('total-compra')
 let contenedorTotalArticulos= document.getElementById('cantidad-articulos')
@@ -80,6 +24,83 @@ let totalCompra = totalCosto();
 contenedorTotalCompra.textContent = `UYU ${totalCompra.toLocaleString('es-ES')}`
 
 });
+
+function cargarCarrito () {
+  let productosCarrito = JSON.parse(localStorage.getItem('producto-carrito'))
+  contenedorProductosCarrito.innerHTML = ''
+  if (!productosCarrito  || productosCarrito.length === 0){
+    contenedorProductosCarrito.innerHTML = `
+    <div class="alert alert-primary text-center">
+    No hay productos en su carrito aún.
+    </div> `
+} else {
+
+  productosCarrito.forEach((productoCarrito, index) => { 
+    contenedorProductosCarrito.innerHTML += `<div class="justify-content-center d-flex">
+    <div class="card">
+    <div class="card position-relative">
+    <i class="fas fa-trash-alt text-secondary position-absolute" id="eliminar-${productoCarrito.id}" style="top: 10px; right: 10px; cursor: pointer;"></i>
+    <div class="row">
+    <div class="col-md-4">
+      <img src="${productoCarrito.image}" class="img-fluid rounded-start h-100" alt="${productoCarrito.name}">
+    </div>
+    <div class="col-md-8 d-flex flex-column justify-content-between">
+      <div class="card-body">
+        <h5 class="card-title">${productoCarrito.name}</h5>
+        <h5 class="card-title">${productoCarrito.currency} ${productoCarrito.cost}</h5>
+        <input type="number" id="cantidad-${productoCarrito.id}" class="form-control w-25" min="1"  value="${productoCarrito.cantidad}">
+        <p class="card-text">Subtotal: <span id="subtotal-${productoCarrito.id}">${productoCarrito.cost}</span></p>
+      </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    `
+
+    
+  });
+
+}
+
+actualizarCarrito ();
+
+}
+
+function actualizarCarrito(){
+  if (productosCarrito){
+    productosCarrito.forEach((productoCarrito, index) => {
+      let inputCantidad = document.getElementById(`cantidad-${productoCarrito.id}`);
+      let subtotalElemento = document.getElementById(`subtotal-${productoCarrito.id}`);
+      let botonEliminar = document.getElementById(`eliminar-${productoCarrito.id}`)
+      
+      //Escucha si se da click en el botón eliminar el artículo
+      botonEliminar.addEventListener('click', function (){
+        productosCarrito.splice(index, 1)
+        localStorage.setItem('producto-carrito', JSON.stringify(productosCarrito))
+        cargarCarrito ();
+        updateTotals ();
+  
+      })
+    
+      // Escucha cambios en el input de cantidad
+      inputCantidad.addEventListener('input', function() {
+          let cantidad = parseInt(inputCantidad.value) || 1; // Asegura que cantidad sea al menos 1
+          let subtotal = productoCarrito.cost * cantidad;
+          subtotalElemento.textContent = subtotal;
+          
+          productosCarrito[index] = { ...productosCarrito[index], cantidad };
+    
+          localStorage.setItem('producto-carrito', JSON.stringify(productosCarrito));
+    
+          updateTotals();
+          
+     });
+     
+    
+    });
+  }
+
+}
 
 function totalCosto() {
   let productosCarrito = JSON.parse(localStorage.getItem('producto-carrito')) || [];
@@ -110,11 +131,6 @@ function updateTotals() {
 
   totalArticulos = totalCarrito();
   totalCompra = totalCosto();
-
-  //productosCarrito.forEach(producto => {
-     // totalArticulos += producto.cantidad;
-      //totalCompra += totalCosto();
- // });
 
   document.getElementById('cantidad-articulos').textContent = totalArticulos > 0 ? totalArticulos : '0';
   document.getElementById('total-compra').textContent = `UYU ${totalCompra.toLocaleString('es-ES')}`;
